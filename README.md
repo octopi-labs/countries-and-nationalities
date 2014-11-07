50362d9a4f28f103c505d6a6655ae19a94d9f8a2
srahul07 <srahul07@gmail.com>
2014-11-07 17:52:37 +0530

    Adding country list in sql and csv format

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 countries
 =========
 
-A list of country and information related to country like is02, iso3, shortname, longname, nationality, numcode, unmember, callingcode, cctld
+A list of country and information related to country like iso2, iso3, shortname, longname, nationality, numcode, unmember, callingcode, cctld
+
+List formats are as follows:
+1. sql
+2. csv
---
+++ country_table_structure.sql
@@ -0,0 +1,49 @@
+-- MySQL dump 10.15  Distrib 10.0.13-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: nationality
+-- ------------------------------------------------------
+-- Server version	10.0.13-MariaDB-1~precise-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `country`
+--
+
+DROP TABLE IF EXISTS `country`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `country` (
+  `id` smallint(5) NOT NULL AUTO_INCREMENT,
+  `iso2` char(2) DEFAULT NULL,
+  `iso3` char(3) DEFAULT NULL,
+  `shortname` varchar(80) NOT NULL DEFAULT '',
+  `longname` varchar(80) NOT NULL DEFAULT '',
+  `nationality` varchar(100) NOT NULL DEFAULT '',
+  `numcode` varchar(6) DEFAULT NULL,
+  `unmember` varchar(12) DEFAULT NULL,
+  `callingcode` varchar(8) DEFAULT NULL,
+  `cctld` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=252 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-11-07 17:43:36
